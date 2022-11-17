eb3a466443d52060cbc31e584a880b68c90cc346
MarkoStahovec <xstahovec@stuba.sk>
2022-11-17 13:38:29 +0100

    boat

--- src/Fish.cpp
+++ src/Fish.cpp
@@ -5,6 +5,8 @@
 #include "Fish.h"
 #include <shaders/diffuse_vert_glsl.h>
 #include <shaders/diffuse_frag_glsl.h>
+#include <shaders/phong_vert_glsl.h>
+#include <shaders/phong_frag_glsl.h>
 
 std::unique_ptr<ppgso::Mesh> Fish::mesh;
 std::unique_ptr<ppgso::Texture> Fish::texture;
@@ -14,7 +16,7 @@ Fish::Fish() {
     scale *= 4.5;
     position = {10,10,10};
 
-    if (!shader) shader = std::make_unique<ppgso::Shader>(diffuse_vert_glsl, diffuse_frag_glsl);
+    if (!shader) shader = std::make_unique<ppgso::Shader>(phong_vert_glsl, phong_frag_glsl);
     if (!texture) texture = std::make_unique<ppgso::Texture>(ppgso::image::loadBMP("fish.bmp"));
     if (!mesh) mesh = std::make_unique<ppgso::Mesh>("fish.obj");
 }
@@ -33,12 +35,26 @@ void Fish::render(SceneWindow &scene) {
     shader->use();
 
     // use camera
-    shader->setUniform("ProjectionMatrix", scene.camera->projectionMatrix);
-    shader->setUniform("ViewMatrix", scene.camera->viewMatrix);
-    shader->setUniform("LightDirection", normalize(glm::vec3{1.0f, 1.0f, 1.0f}));
+    shader->setUniform("projection", scene.camera->projectionMatrix);
+    shader->setUniform("view", scene.camera->viewMatrix);
+    shader->setUniform("model", modelMatrix);
+
+    shader->setUniform("light.position", normalize(glm::vec3{1.2f, 1.0f, 2.0f}));
+    shader->setUniform("viewPos", scene.camera->cameraPos);
+
+    shader->setUniform("light.ambient", {0.2f,0.2f,0.2f});
+    shader->setUniform("light.diffuse", {0.5f,0.5f,0.5f});
+    shader->setUniform("light.specular", {1.0f,1.0f,1.0f});
+    shader->setUniform("light.constant", 1.0f);
+    shader->setUniform("light.linear", 0.01f);
+    shader->setUniform("light.quadratic", 0.0f);
+
+    shader->setUniform("material.diffuse", 0);
+    shader->setUniform("material.specular", 1);
+    shader->setUniform("material.shininess", 32.0f);
+
 
     // render mesh
-    shader->setUniform("ModelMatrix", modelMatrix);
     shader->setUniform("Texture", *texture);
     mesh->render();
 }
